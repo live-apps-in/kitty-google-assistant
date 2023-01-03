53bfd65c72483224cdb159304fcae429850b5dd4
Jaga <jagadheesh6@gmail.com>
2023-01-03 17:07:47 +0530

    chore  - Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,13 @@ server.use(cors());
 ///Listen for Google Assistant Events
 server.post('/fulfillment', app);
 
+server.get('/', (req, res) => {
+	res.send({
+		app: 'kitty chan Google Assistant',
+		port: 5006
+	})
+})
+
 ////Start Express Server
 server.listen(5000, () => {
 	console.log('Listening on port 5000');
